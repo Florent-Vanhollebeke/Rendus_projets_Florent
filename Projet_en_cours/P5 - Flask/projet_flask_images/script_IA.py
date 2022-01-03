a605c1826205172e25b9dcb6ca9b8b6aa9b52574
Florent-Vanhollebeke <florent.vanhollebeke@gmail.com>
2022-01-03 13:17:44 +0100

    modif flask

--- Projet_en_cours/P5 - Flask/projet_flask_images/script_IA.py
+++ Projet_en_cours/P5 - Flask/projet_flask_images/script_IA.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import pandas as pd
+import matplotlib.pyplot as plt
 import keras
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -9,12 +10,23 @@ import pickle
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-#division par 255 pour avoir des valeurs entre 0 et 1
-X_train = X_train.reshape(X_train.shape[0],-1)/255 
-X_test = X_test.reshape(X_test.shape[0],-1)/255
+def afficher_images(nb_images_a_afficher, nb_colonnes=5):
+    nb_lignes = nb_images_a_afficher//nb_colonnes+1
+    plt.figure(figsize=(20,5*nb_lignes))
+    for index, (image, label) in enumerate(zip(X_train[0:nb_images_a_afficher], y_train[0:nb_images_a_afficher])):
+        plt.subplot(nb_lignes, nb_colonnes, index + 1)
+        plt.imshow(image)
+        plt.title('Training: %i\n' % label, fontsize = 15)
+afficher_images(15)
+
+
+# reshape des images en vecteur
+X_train = X_train.reshape(60000,-1)
+X_test = X_test.reshape(10000,-1)
 
 model = LogisticRegression(solver='lbfgs', max_iter=1000, n_jobs=8)
 model.fit(X_train, y_train)
+model.score(X_test,y_test)
 #y_pred = reglog.predict(X_test)
 
 pickle.dump(model, open('mnist.sav', 'wb'))
