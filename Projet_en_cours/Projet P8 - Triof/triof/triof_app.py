c5096aedb774c91d16c70efb31c8b1a82260ad85
Florent-Vanhollebeke <florent.vanhollebeke@gmail.com>
2022-05-04 11:25:00 +0200

    modif du fichier triof_app.py

--- Projet_en_cours/Projet P8 - Triof/triof/triof_app.py
+++ Projet_en_cours/Projet P8 - Triof/triof/triof_app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for
+#from requests import session
 from src.utils import *
 from acces_azure import *
 import cv2
@@ -11,9 +12,11 @@ import os
 import random
 from matplotlib.image import imread
 
+from script_clean_or_dirty import *
+
+
 app = Flask(__name__)
-
-
+app.secret_key = 'BAD_SECRET_KEY'
 
 @app.route('/')
 def home():
@@ -26,22 +29,24 @@ def insert():
     paths = os.listdir('test')
     path = random.choice(paths)
     pics = take_trash_picture(path)
-    prediction = first_prediction_azure('test/'+path)
+    pred = first_prediction_azure('test/'+path)
+    session['solution'] = pred
     PIL_image = Image.fromarray(np.uint8(pics)).convert('RGB')
+    PIL_image2 = PIL_image.resize((256,256))
+    PIL_image2.save("{PIL_image}.jpg", "JPEG", optimize=True)
     data = BytesIO()
-    PIL_image.save(data, "JPEG")
+    PIL_image2.save(data, "JPEG")
     data64 = b64encode(data.getvalue())
-    PIL_image = u'data:img/jpeg;base64,'+data64.decode('utf-8') 
-    return render_template('insert.html', pics = PIL_image, prediction = prediction)
-
+    PIL_image2 = u'data:img/jpeg;base64,'+data64.decode('utf-8') 
+    return render_template('insert.html', pics = PIL_image2, prediction = pred)
 
 
 @app.route('/waste/pick-type')
 def pick_type():
     close_waste_slot()
-    
+    message = session.get('solution')
 
-    return render_template('type.html')
+    return render_template('type.html',message=message)
 
 
 @app.route('/confirmation', methods=['POST'])
@@ -50,6 +55,45 @@ def confirmation():
 
     process_waste(waste_type)
     return render_template('confirmation.html')
+
+
+
+
+
+@app.route('/propre')
+def clean_dirty():
+    open_waste_slot()
+    paths = os.listdir('test')
+    path = random.choice(paths)
+    pics = take_trash_picture(path)
+    pred = clean_or_dirty('test/'+path)
+    session['solution'] = pred
+    PIL_image = Image.fromarray(np.uint8(pics)).convert('RGB')
+    PIL_image2 = PIL_image.resize((256,256))
+    PIL_image2.save("{PIL_image}.jpg", "JPEG", optimize=True)
+    data = BytesIO()
+    PIL_image2.save(data, "JPEG")
+    data64 = b64encode(data.getvalue())
+    PIL_image2 = u'data:img/jpeg;base64,'+data64.decode('utf-8') 
+    return render_template('resultat.html', pics = PIL_image2, prediction = pred)
+
+
+@app.route('/suite')
+def suite_proprete():
+    close_waste_slot()
+    message = session.get('solution')
+
+    return render_template('proprete.html',message=message)
+
+@app.route('/remerciement', methods=['POST'])
+def remerciement():
+    waste_type = request.form['proprete']
+
+    process_waste(waste_type)
+    return render_template('confirmation.html')
+
+
+
 
 
 if __name__ == "__main__":
